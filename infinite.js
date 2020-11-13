37bb3b8c89eb7eb2e8d73ed315a2d300b4ded38c
stewpidtnlvr <61797693+stewpidtnlvr@users.noreply.github.com>
2020-11-13 13:28:29 -0600

    Update infinite.js

--- infinite.js
+++ infinite.js
@@ -56,7 +56,7 @@ function racer(gamemode) {
     var difficultyMax = 14;                      // Maximum difficulty, after this there will be no increase in difficulty
     var difficultyCurrent = difficultyStart;    // Current difficulty (will be modified ingame)
     var remainingTimeIncrease = 1;                      // Multiplier of the trackLength to get seconds that will be added to the remainingTime, in other words this defines the time left to the player to finish the track proportionally to the track length (a higher value makes the game easier)
-    var remainingTimeIncrease = remainingTimeIncrease +1;                      // make time infinite
+   /* var remainingTimeIncrease = remainingTimeIncrease +1;                      // make time infinite*/
     var remainingTimeStartBonus = 2.0;                      // Multiplier of the remaining time given for the first level (to make game easier for newscomers), also because the player has no momentum at the beginning
     var remainingTimeThreshold = 20;      // When only this amount of time is left, the remaining time HUD will be highlighted (set to 0 to disable)
     var currentLevel = 0;                           // Internal variable, just a value to display the current level
@@ -222,12 +222,12 @@ function racer(gamemode) {
             // Increase level (only useful for display, internally we use difficultyCurrent)
             currentLevel += 1;
             // Give the player some more time
-            var remainingTimePast = remainingTime;
+           /* var remainingTimePast = remainingTime;
             remainingTime += trackLength * remainingTimeIncrease;
             if ((remainingTimePast < remainingTimeThreshold) & (remainingTime > remainingTimeThreshold)) {
                 Dom.removeClassName('remaining_time_value', 'warninglow'); // remove any warning if there was one
                 Dom.addClassName('remaining_time_value', 'value');
-            }
+            }*/
             // Increase current difficulty unless we are already at the max
             if (difficultyCurrent < difficultyMax) {
                 difficultyCurrent += difficultyIncrement;
@@ -256,7 +256,7 @@ function racer(gamemode) {
           } else { // fastest lap time gamemode
               lastLapTime    = currentLapTime;
               currentLapTime = 0;
-              if ((lastLapTime <= Util.toFloat(Dom.storage.fast_lap_time)) | (Util.toFloat(Dom.storage.fast_lap_time) == 0)) {
+      /*        if ((lastLapTime <= Util.toFloat(Dom.storage.fast_lap_time)) | (Util.toFloat(Dom.storage.fast_lap_time) == 0)) {
                 Dom.storage.fast_lap_time = lastLapTime;
                 updateHud('fast_lap_time', formatTime(lastLapTime));
                 Dom.addClassName('fast_lap_time', 'fastest');
@@ -278,7 +278,7 @@ function racer(gamemode) {
             if (currentLevel == 0) { // first level, we give some time to the player
                 remainingTime += trackLength * remainingTimeIncrease * remainingTimeStartBonus;
             }
-          }
+          }*/
 
           // Highlight remaining time if quite low
           if ((gamemode == 1) & (remainingTime < remainingTimeThreshold)) {
